da07b7e2d328ba271727680058de2392966760f5
Muhammad Shaheer Khan <k250755@nu.edu.pk>
2025-11-13 22:31:03 +0500

    Create shuffling in array.c

---
+++ shuffling in array.c
@@ -0,0 +1,12 @@
+int* shuffle(int* nums, int numsSize, int n, int* returnSize) {
+    *returnSize = numsSize;
+    int* result = (int*)malloc(numsSize * sizeof(int));
+    
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+        result[j++] = nums[i];   
+        result[j++] = nums[i + n]; 
+    }
+    
+    return result;
+}
